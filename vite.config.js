87bfd4f492e0993af7a46608252479ff6987dfe6
alexrosenberg650 <144967559+alexrosenberg650@users.noreply.github.com>
2025-04-02 18:13:59 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: "./", // ← THIS is the important fix
   plugins: [react()],
-  css: { postcss: './postcss.config.js' },
-  server: { host: true, port: 5173, allowedHosts: 'all' }
+  css: {
+    postcss: './postcss.config.js'
+  },
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: 'all'
+  }
 });
