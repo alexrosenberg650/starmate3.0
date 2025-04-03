2e796c59d6d51cada6b63eda96ff1cc64e781591
alexrosenberg650 <144967559+alexrosenberg650@users.noreply.github.com>
2025-04-03 01:26:32 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   base: './',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html') // 👈 explicitly define the entry
+    }
+  },
   server: {
     host: true,
     port: 5173,
     allowedHosts: 'all'
   }
 });
+
